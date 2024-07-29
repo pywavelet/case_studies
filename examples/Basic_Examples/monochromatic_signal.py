af46f8ced4633911b4db30a37f997a4f402b13a9
Alexander Burke <ollie0burke@gmail.com>
2024-07-29 22:11:22 +0200

    tidied up, ready for use

--- examples/Basic_Examples/monochromatic_signal.py
+++ examples/Basic_Examples/monochromatic_signal.py
@@ -1,27 +1,20 @@
+from tkinter import W
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-sys.path.append("../../../")
-import pytest
-import pywavelet
+from scipy.signal.windows import tukey
 from pywavelet.data import Data
 from pywavelet.psd import evolutionary_psd_from_stationary_psd
 from pywavelet.transforms.to_wavelets import from_time_to_wavelet, from_freq_to_wavelet
-from pywavelet.transforms.from_wavelets import (from_wavelet_to_time, 
-                                                from_wavelet_to_freq, 
-                                                from_wavelet_to_freq_to_time)
 from pywavelet.transforms.types import FrequencySeries, TimeSeries
-from pywavelet.utils.lisa import get_lisa_data
-from pywavelet.utils.lvk import inject_signal_in_noise
 from pywavelet.utils.snr import compute_snr
-
-
+from pywavelet.plotting import plot_wavelet_grid
 
 f0 = 20
 T = 1000
 A = 2.0
 PSD_AMP = 1e-2
-Nf = 256
+Nf = 512
 
 dt = 0.5 / (2 * f0)  # Shannon's sampling theorem, set dt < 1/2*highest_freq
 t = np.arange(0, T, dt)  # Time array
@@ -31,6 +24,8 @@ T = len(t) * dt
 ND = len(t)
 
 y = A * np.sin(2 * np.pi * f0 * t)  # Signal waveform we wish to test
+window = tukey(len(y), 0.0) # Essential if you want to remove leakage
+y *= window # Apply taper
 freq = np.fft.rfftfreq(len(y), dt)  # Frequencies
 df = abs(freq[1] - freq[0])  # Sample spacing in frequency
 y_fft = np.fft.rfft(y)  # continuous time fourier transform [seconds]
@@ -51,6 +46,7 @@ Nt = ND // Nf
 signal_timeseries = TimeSeries(y, t)
 signal_frequencyseries = FrequencySeries(y_fft,freq = freq)
 
+# From time to wavelet
 signal_wavelet_time = from_time_to_wavelet(signal_timeseries, Nf = Nf)
 psd_wavelet_time= evolutionary_psd_from_stationary_psd(
     psd=PSD,
@@ -60,6 +56,7 @@ psd_wavelet_time= evolutionary_psd_from_stationary_psd(
     dt=dt,
 )
 
+# From frequency to wavelet
 signal_wavelet_freq = from_freq_to_wavelet(signal_frequencyseries, Nf = Nf)
 psd_wavelet_freq= evolutionary_psd_from_stationary_psd(
     psd=PSD,
@@ -68,6 +65,8 @@ psd_wavelet_freq= evolutionary_psd_from_stationary_psd(
     t_grid=signal_wavelet_freq.time,
     dt=dt,
 )
+
+# Compute SNR in wavelet domain
 snr2_time_to_wavelet = compute_snr(signal_wavelet_time, psd_wavelet_time) ** 2
 snr2_freq_to_wavelet = compute_snr(signal_wavelet_freq, psd_wavelet_freq) ** 2
 
@@ -80,36 +79,21 @@ print("SNR using wavelet domain, freq -> wavelet", snr2_freq_to_wavelet**(1/2))
 
 # Does this actually make sense?
 
-from pywavelet.plotting import plot_wavelet_grid
 
-# fig,ax = plt.subplots(2,1)
-# plot_wavelet_grid(signal_wavelet_freq.data,
-#                 time_grid=signal_wavelet_freq.time,
-#                 freq_grid=signal_wavelet_freq.freq,
-#                 ax=None,
-#                 zscale="linear",
-#                 freq_scale="linear",
-#                 absolute=False,
-#                 freq_range=[18,22])
-# plt.clf()
+fig,ax = plt.subplots(2,1)
+plot_wavelet_grid(signal_wavelet_freq.data,
+                time_grid=signal_wavelet_freq.time,
+                freq_grid=signal_wavelet_freq.freq,
+                ax=None,
+                zscale="linear",
+                freq_scale="linear",
+                absolute=False,
+                freq_range=[18,22])
+plt.show()
+plt.clf()
 
 print("Maximum value at f = {} is {} using freq -> wavelet".format(f0, max(signal_wavelet_freq.data.flatten())))
 print("Maximum value at f = {} is {} using time -> wavelet".format(f0, max(signal_wavelet_time.data.flatten())))
-# Ollie's bullshit formula
+# Ollie's analytical guess
 print("Hypothesis for true wavelet domain transformation is {}".format(A*np.sqrt(2*Nf)))
-
-# Now try to work out inverse formulas
-
-
-# All of these work now!
-
-signal_freq_from_wavelet = from_wavelet_to_freq(signal_wavelet_freq, dt)
-# signal_time_from_wavelet = from_wavelet_to_time(signal_wavelet_time, dt)
-# signal_time_from_wavelet = from_wavelet_to_freq_to_time(signal_wavelet_time, dt)
-breakpoint()
-print("max value of periodigram from wavelet -> freq", np.max(np.abs(signal_freq_from_wavelet.data)**2))
-print("We expect a value of ", A**2 * ND**2 / 4)
-breakpoint()
-
-plt.show()
 
