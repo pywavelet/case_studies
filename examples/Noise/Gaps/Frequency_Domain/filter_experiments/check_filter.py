1a858535e6f3c831fd4c53781e518b9a2a663f94
Alexander Burke <ollie0burke@gmail.com>
2024-08-05 01:04:43 +0200

    finished experimenting. sos way to go

--- examples/Noise/Gaps/Frequency_Domain/filter_experiments/check_filter.py
+++ examples/Noise/Gaps/Frequency_Domain/filter_experiments/check_filter.py
@@ -1,71 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import (butter, filtfilt, freqz, sosfilt, sosfreqz,
-                          freqz_zpk)
+from scipy.signal import (butter, filtfilt, freqz, sosfilt, sosfiltfilt, sosfreqz,
+                          freqz_zpk, tf2zpk, sos2zpk)
 from scipy.signal.windows import tukey
 from scipy.signal import chirp
-
-# Define the bandpass_data function
-def bandpass_data_ba(rawstrain, f_min_bp, fs, bandpassing_flag=False, order=4):
-    if bandpassing_flag:
-        f_nyq = 0.5 * fs
-        b, a = butter(order, f_min_bp / f_nyq, btype='highpass', output = 'ba')
-        strain = filtfilt(b, a, rawstrain)
-    else:
-        strain = rawstrain
-    return strain
-
-def bandpass_data_sos(rawstrain, f_min_bp, fs, bandpassing_flag=False, order=4):
-    if bandpassing_flag:
-        f_nyq = 0.5 * fs
-        sos = butter(order, f_min_bp / f_nyq, btype='highpass', output = 'sos')
-        strain = sosfilt(sos, rawstrain)
-    else:
-        strain = rawstrain
-    return strain
-
-# Define the get_frequency_response function
-def get_frequency_response_ba(f_min_bp, fs, freqs, order=4):
-    # N_freq = N // 2 + 1
-    f_nyq = 0.5 * fs
-
-    b, a = butter(order, f_min_bp / f_nyq, btype='highpass', output = 'ba')
-    
-    # Compute the frequency response
-    w, h = freqz(b, a, worN=freqs * 2 * np.pi / fs)
-    
-    # Convert w to actual frequency in Hz
-    freqs = w * fs / (2 * np.pi)
-    
-    return freqs, h
-
-def get_frequency_response_sos(f_min_bp, fs, freqs, order=4):
-    # N_freq = N // 2 + 1
-    f_nyq = 0.5 * fs
-
-    sos = butter(order, f_min_bp / f_nyq, btype='highpass', output = 'sos')
-    
-    # Compute the frequency response
-    w, h = sosfreqz(sos, worN=freqs * 2 * np.pi / fs, whole=False, fs=fs)
-    
-    # Convert w to actual frequency in Hz
-    freqs = w * fs / (2 * np.pi)
-    
-    return freqs, h
-# Define the get_frequency_response function
-def get_frequency_response_zpk(f_min_bp, fs, freqs, order=4):
-    # N_freq = N // 2 + 1
-    f_nyq = 0.5 * fs
-
-    z,p,k = butter(order, f_min_bp / f_nyq, btype='highpass', output = 'zpk')
-    
-    # Compute the frequency response
-    w, h = freqz_zpk(z,p,k, worN=freqs * 2 * np.pi / fs, whole=False, fs=fs)
-    
-    # Convert w to actual frequency in Hz
-    freqs = w * fs / (2 * np.pi)
-    
-    return freqs, h
 
 # Parameters
 f0 = 50
@@ -102,59 +40,74 @@ freq = np.fft.rfftfreq(len(y), dt)
 f_low_index = np.argwhere(freq >= 100)[0][0]
 
 # Set up filters
-order = 6
+order = 5 
 
+# Standard (b,a) format. Not great for high orders. Numerically unstable. 
 b, a = butter(order, freq[f_low_index] / f_nyq, btype='highpass', output = 'ba')
-sos = butter(order,  freq[f_low_index] / f_nyq, btype='highpass', output = 'sos')
-z,p,k = butter(order,  freq[f_low_index] / f_nyq, btype='highpass', output = 'zpk')
+z_from_ab, p_from_ab, k_from_ab = tf2zpk(b,a)
+print("Check the poles of coefficients of b, a in butter")
+print(np.sort(abs(p_from_ab)))
 
-# Apply the filters
+# second order cascade format. Much more stable. 
+sos = butter(order,  freq[f_low_index] / f_nyq, btype='highpass', output = 'sos')
+z_from_sos, p_from_sos, k_from_sos = sos2zpk(sos)
+print("Now, from the second order cascading method (sos), check poles")
+print(np.sort(abs(p_from_sos)))
+
+# Another method, very stable. 
+z,p,k = butter(order,  freq[f_low_index] / f_nyq, btype='highpass', output = 'zpk')
+print("converting straight to zpk format gives poles:")
+print(np.sort(abs(p)))
+
+# Apply the filters, forward and backward filters
 y_filtered_ba = filtfilt(b, a, y)
-y_filtered_sos = sosfilt(sos, y)
+y_filtered_sos = sosfiltfilt(sos, y)
+
 # Compute the frequency response
-# freqs_ba, response_f_ba, = freqz(b, a, worN= freq * 2 * np.pi / fs, whole = False, fs = fs)
 freqs_ba, response_f_ba, = freqz(b, a, worN = N//2 + 1 , whole = False, fs = fs)
 freqs_sos, response_f_sos = sosfreqz(sos, worN= N//2 + 1, whole=False, fs=fs)
 freqs_zpk, response_f_zpk = freqz_zpk(z,p,k, worN= N//2 + 1 , whole=False, fs=fs) 
 
 # Apply the frequency response to the window function in the frequency domain
-
-check_filter_ba = response_f_ba * np.fft.rfft(y)
-check_filter_sos = response_f_sos * np.fft.rfft(y)
-check_filter_zpk = response_f_zpk * np.fft.rfft(y)
+# If we use filtfilt then we filter forwards and then backwards in time. This
+# removes any phase offset. The true response function in freq domain is the
+# squared response abs(response_f)**2. 
+check_filter_ba = abs(response_f_ba)**2 * np.fft.rfft(y)
+check_filter_sos = abs(response_f_sos)**2 * np.fft.rfft(y)
+check_filter_zpk = abs(response_f_zpk)**2  * np.fft.rfft(y)
+# If we filter just forwards, then we just need response_f_ba * ...
 
 # Compute the FFT of the filtered window function
-filtered_w_fft_ba = np.fft.rfft(y_filtered_ba)
-filtered_w_fft_sos = np.fft.rfft(y_filtered_sos)
+filtered_y_fft_ba = np.fft.rfft(y_filtered_ba)
+filtered_y_fft_sos = np.fft.rfft(y_filtered_sos)
 
-breakpoint()
 # Normalize frequency response multiplication
 fig, ax = plt.subplots(1,3, figsize=(18, 6))
 ax[0].loglog(freq, np.abs(check_filter_ba)**2, c = 'blue', label='K(f) * W(f)')
-ax[0].loglog(freq, np.abs(filtered_w_fft_ba)**2, linestyle = '--', c = 'red', label='Filtered w(t)')
+ax[0].loglog(freq, np.abs(filtered_y_fft_ba)**2, linestyle = '--', c = 'red', label='Filtered w(t)')
 ax[0].loglog(freq, np.abs(response_f_ba)**2, label = 'Filter response')
 ax[0].legend(loc = 'lower left')
 ax[0].set_xlabel('Frequency [Hz]')
 ax[0].set_ylabel('Power')
-ax[0].set_title('Using standard butter with (b,a) format')
+ax[0].set_title('Using butter filtfilt with (b,a) format at order {}'.format(order), fontsize = 10)
 ax[0].grid()
 # Normalize frequency response multiplication
 ax[1].loglog(freq, np.abs(check_filter_sos)**2, c = 'blue', label='K(f) * W(f)')
-ax[1].loglog(freq, np.abs(filtered_w_fft_sos)**2, c = 'red', linestyle = '--', label='Filtered w(t)')
+ax[1].loglog(freq, np.abs(filtered_y_fft_sos)**2, c = 'red', linestyle = '--', label='Filtered w(t)')
 
 ax[1].loglog(freq, np.abs(response_f_sos)**2, label = 'Filter response')
 ax[1].legend(loc='lower left')
 ax[1].set_xlabel('Frequency [Hz]')
 ax[1].set_ylabel('Power')
-ax[1].set_title('Using standard butter with sos format')
+ax[1].set_title('Using butter with sosfiltfilt format at order {}'.format(order), fontsize = 10)
 ax[1].grid()
 
 ax[2].loglog(freq, np.abs(check_filter_zpk)**2, c = 'blue', label='K(f) * W(f)')
-ax[2].loglog(freq, np.abs(filtered_w_fft_sos)**2, c = 'red', linestyle = '--', label='Filtered w(t)')
+ax[2].loglog(freq, np.abs(filtered_y_fft_sos)**2, c = 'red', linestyle = '--', label='Filtered w(t)')
 ax[2].loglog(freq, np.abs(response_f_zpk)**2, label = 'Filter response')
 ax[2].legend(loc='lower left')
 ax[2].set_xlabel('Frequency [Hz]')
 ax[2].set_ylabel('Power')
-ax[2].set_title('Using standard butter with zpk format')
+ax[2].set_title('Using butter sosfiltfilt with zpk format at order {}'.format(order), fontsize = 10)
 ax[2].grid()
 plt.show()
