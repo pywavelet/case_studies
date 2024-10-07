56e2adb12349023e8dcb5c344ad45ef8d6966ce3
Alexander Burke <ollie0burke@gmail.com>
2024-10-07 12:36:25 +0200

    changed imports

--- examples/Basic_Examples/monochromatic_signal.py
+++ examples/Basic_Examples/monochromatic_signal.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 from scipy.signal.windows import tukey
-from pywavelet.data import Data
-from pywavelet.psd import evolutionary_psd_from_stationary_psd
+# from pywavelet.data import Data
+from pywavelet.utils import evolutionary_psd_from_stationary_psd
 from pywavelet.transforms.to_wavelets import from_time_to_wavelet, from_freq_to_wavelet
 from pywavelet.transforms.types import FrequencySeries, TimeSeries
-from pywavelet.utils.snr import compute_snr
-from pywavelet.plotting import plot_wavelet_grid
+from pywavelet.utils import compute_snr
+# from pywavelet.plotting import plot_wavelet_grid
 
 f0 = 20
 T = 1000
 A = 2.0
 PSD_AMP = 1e-2
-Nf = 510
+Nf = 512
 
 dt = 0.5 / (2 * f0)  # Shannon's sampling theorem, set dt < 1/2*highest_freq
 t = np.arange(0, T, dt)  # Time array
@@ -22,7 +22,6 @@ t = np.arange(0, T, dt)  # Time array
 t = t[: 2 ** int(np.log2(len(t)))]
 T = len(t) * dt
 ND = len(t)
-breakpoint()
 
 y = A * np.sin(2 * np.pi * f0 * t)  # Signal waveform we wish to test
 window = tukey(len(y), 0.0) # Essential if you want to remove leakage
@@ -81,17 +80,17 @@ print("SNR using wavelet domain, freq -> wavelet", snr2_freq_to_wavelet**(1/2))
 # Does this actually make sense?
 
 
-fig,ax = plt.subplots(2,1)
-plot_wavelet_grid(signal_wavelet_freq.data,
-                time_grid=signal_wavelet_freq.time,
-                freq_grid=signal_wavelet_freq.freq,
-                ax=None,
-                zscale="linear",
-                freq_scale="linear",
-                absolute=False,
-                freq_range=[18,22])
-plt.show()
-plt.clf()
+# fig,ax = plt.subplots(2,1)
+# plot_wavelet_grid(signal_wavelet_freq.data,
+#                 time_grid=signal_wavelet_freq.time,
+#                 freq_grid=signal_wavelet_freq.freq,
+#                 ax=None,
+#                 zscale="linear",
+#                 freq_scale="linear",
+#                 absolute=False,
+#                 freq_range=[18,22])
+# plt.show()
+# plt.clf()
 
 print("Maximum value at f = {} is {} using freq -> wavelet".format(f0, max(signal_wavelet_freq.data.flatten())))
 print("Maximum value at f = {} is {} using time -> wavelet".format(f0, max(signal_wavelet_time.data.flatten())))
