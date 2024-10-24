37bc7e45442e0b163a80751b559d459700f6dbb8
Alexander Burke <ollie0burke@gmail.com>
2024-10-24 13:02:50 +0100

    changed prior range

--- examples/Noise/Gaps/study/Wavelet_Domain/nan_method/constants.py
+++ examples/Noise/Gaps/study/Wavelet_Domain/nan_method/constants.py
@@ -5,17 +5,25 @@ from bilby.core.prior import Uniform, TruncatedGaussian, Gaussian, PriorDict
 ONE_HOUR = 60 * 60
 ONE_DAY = 24 * ONE_HOUR
 A_TRUE = 1e-21
-LN_F_TRUE = np.log(3e-3)
-LN_FDOT_TRUE = np.log(1e-8)
+F_TRUE = 3e-3
+FDOT_TRUE = 1e-8
+
+LN_F_TRUE = np.log(F_TRUE)
+LN_FDOT_TRUE = np.log(FDOT_TRUE)
+
 TRUES = [A_TRUE, LN_F_TRUE, LN_FDOT_TRUE]
 
 NF = 64
 TMAX = 4 * ONE_DAY
 START_GAP = TMAX  * 0.4
 END_GAP = START_GAP + 6 * ONE_HOUR
-A_RANGE = [9e-22, 1e-20]
-LN_F_RANGE = [LN_F_TRUE-0.2, LN_F_TRUE+0.2]
-LN_FDOT_RANGE = [LN_FDOT_TRUE-0.2, LN_FDOT_TRUE+0.2]
+
+A_RANGE = [1e-22, 3e-21]
+F_RANGE = [F_TRUE - 6e-7, F_TRUE + 6e-7]
+FDOT_RANGE = [FDOT_TRUE - 6e-12, FDOT_TRUE + 6e-12]
+
+LN_F_RANGE = [np.log(F_RANGE[0]), np.log(F_RANGE[1])]
+LN_FDOT_RANGE = [np.log(FDOT_RANGE[0]), np.log(FDOT_RANGE[1])]
 
 A_SCALE = A_RANGE[1] - A_RANGE[0]
 LN_F_SCALE = LN_F_RANGE[1] - LN_F_RANGE[0]
