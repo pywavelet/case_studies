8d45ead18494bd972398fc0b70b6f114137fd449
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-11-05 11:28:36 +1300

    clean matrix plotting code

--- examples/Noise/Gaps/study/Wavelet_Domain/nan_method/plot_cov_matrix.py
+++ examples/Noise/Gaps/study/Wavelet_Domain/nan_method/plot_cov_matrix.py
@@ -14,4 +14,20 @@ cbar.ax.tick_params(labelsize=10)
 
 
 plt.tight_layout() 
-plt.show()
+plt.savefig("matrix_directory/Cov_Matrix_Flat_w_filter.png")
+
+
+Cov_Matrix = np.load("matrix_directory/Cov_Matrix_Flat_w_filter_gap.npy")
+
+# title_label = [r'Analytical $|\Sigma_{N}(f,f^{\prime})|$']
+
+fig, ax = plt.subplots(figsize = (14,10))
+mat_gap_mine = ax.matshow(np.log10(np.abs(Cov_Matrix)))#, vmin = -80, vmax = -50)
+cbar = fig.colorbar(mat_gap_mine, ax=ax, location='right',
+             shrink=0.8)
+# cbar.set_label(fontsize = 10)
+cbar.ax.tick_params(labelsize=10)
+
+
+plt.tight_layout()
+plt.savefig("matrix_directory/Cov_Matrix_Flat_w_filter_gap.png")
