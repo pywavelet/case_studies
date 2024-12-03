661cca5ec965932ccea615a33a97a4cff1e4a417
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-12-03 18:17:01 +1300

    speed up tests

--- examples/Noise/Gaps/tests/test_mcmc.py
+++ examples/Noise/Gaps/tests/test_mcmc.py
@@ -1,23 +1,25 @@
-import numpy as np
-
 from gap_study_utils.constants import DT
 from gap_study_utils.mcmc_runner import run_mcmc
 
 
 def test_mcmc(plot_dir):
-
-    filtering_kwgs = dict(
+    ## Note -- these settings are just to test that the MCMC runs without error
+    # the signal is quite shit...
+    # for a proper MCMC test (more iterations) look at
+    # study/Wavelet_Domain/nan_method/mcmc.py
+    kwgs = dict(
         noise_realisation=True,
         alpha=0.1,
         highpass_fmin=0.0001,
         dt=DT,
         tmax=540672,
     )
+    run_mcmc(n_iter=200, outdir=f"{plot_dir}/gap_mcmc", **kwgs)
 
-    run_mcmc(n_iter=250, outdir=f"{plot_dir}/gap_mcmc", **filtering_kwgs)
+    # No gap
     run_mcmc(
-        n_iter=250,
+        n_iter=200,
         gap_ranges=None,
-        outdir=f"{plot_dir}/mcmc",
-        **filtering_kwgs,
+        outdir=f"{plot_dir}/basic_mcmc",
+        **kwgs,
     )
